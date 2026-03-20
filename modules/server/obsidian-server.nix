48a6e0c5f37eb7bb2348e0377ad52d80a223cdc5
jvmitt <jvtrschmitt404@gmail.com>
2026-03-20 09:36:01 -0300

    obsidian-server

--- modules/server/obsidian-server.nix
+++ modules/server/obsidian-server.nix
@@ -32,19 +32,19 @@ in
           EOF
           }
 
-          cd ${dir}
+          cd "''${dir}"
 
-          date_yesterday = date -d "-1 day" +%Y-%m-$d
+          date_yesterday = $(date -d "-1 day" +%Y-%m-%d)
 
-          if [ -f ${tasks_file} ]; then
-             mv ${tasks_file} ./.history/tasks/tasks_"$(date_yesterday)".md
+          if [ -f ''${tasks_file} ]; then
+             mv "''${tasks_file}" ./.history/tasks/tasks_$"date_yesterday".md
           else
-             generate_tasks_file ${tasks_file}
+             generate_tasks_file "''${tasks_file}"
           fi
 
-          if [ -f ${tomorrow_tasks_file} ]; then
-             mv ${tomorrow_tasks_file} ${tasks_file}
-             generate_tasks_file ${tomorrow_tasks_file}
+          if [ -f "''${tomorrow_tasks_file}" ]; then
+             mv "''${tomorrow_tasks_file}" "''${tasks_file}"
+             generate_tasks_file "''${tomorrow_tasks_file}"
           fi
 
         '';
